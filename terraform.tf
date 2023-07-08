6cdff5ba490eac1a855737b979eebc7f9cbae711
datta <dattakolekar1002@gmail.com>
2023-07-08 19:51:07 +0530

    --- Auto Git Commit ---

---
+++ terraform.tf
@@ -0,0 +1,9 @@
+provider "aws"{
+    region = ap-southeast-1
+}
+resource "aws_instance" "my_instance" {
+    ami = ami-00d785f1c099d5a0e
+    instance_type= = t2.medium
+    key_name = dev-key-pair
+    tags = datta 
+}    
