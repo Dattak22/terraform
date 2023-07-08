9dea9946ad3c302c15ddd0099f4a54b553e57896
datta <dattakolekar1002@gmail.com>
2023-07-08 19:49:57 +0530

    --- Auto Git Commit ---

--- terraform.tf
+++ terraform.tf
@@ -4,5 +4,13 @@ provider "aws"{
 resource "aws_instance" "my_instance" {
     ami = ami-00d785f1c099d5a0e
     instance_type= = t2.medium
-    key_name = dev
+    key_name = dev-keyresource template_dir name {
+      source_dir      = sourcepath
+      destination_dir = destinationpath
+    
+      vars = {
+        var = value
+      }
+    }
+    
 }
